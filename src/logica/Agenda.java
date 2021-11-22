55ae28f8b40a5fc6ea129b9a11536552fefa3c57
juliangiral <juliangiral97@gmail.com>
2021-11-22 14:44:35 -0300

    Confirmo.

---
+++ src/logica/Agenda.java
@@ -0,0 +1,17 @@
+package logica;
+
+import igu.Pantalla;
+
+class variables {
+    static String dni[]=new String[10];
+}
+
+public class Agenda {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
